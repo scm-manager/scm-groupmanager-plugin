7c01b60603def893e2920f147f96fd55d2672664
Mohamed Karray <mohamed.karray.sr@gmail.com>
2019-01-21 11:34:37 +0100

    add GroupManagerRealm and refactor GroupManagerService

--- src/main/java/sonia/scm/groupmanager/service/GroupManagerService.java
+++ src/main/java/sonia/scm/groupmanager/service/GroupManagerService.java
@@ -6,31 +6,40 @@ import sonia.scm.store.ConfigurationStore;
 import sonia.scm.store.ConfigurationStoreFactory;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
 
+
+@Singleton
 public class GroupManagerService {
 
   public static final String PERMISSION = "group:manage";
   public static final String STORE_NAME = "groupManager";
-  private AllGroupManagers allGroupManagers;
   private final ConfigurationStore<AllGroupManagers> store;
 
   @Inject
   public GroupManagerService(ConfigurationStoreFactory storeFactory) {
     store = storeFactory.withType(AllGroupManagers.class).withName(STORE_NAME).build();
-    allGroupManagers = store.get();
+  }
+
+  private AllGroupManagers getAllGroupManagers() {
+    AllGroupManagers allGroupManagers = store.get();
     if (allGroupManagers == null) {
       allGroupManagers = new AllGroupManagers();
       store.set(allGroupManagers);
     }
+    return allGroupManagers;
   }
 
   public void set(GroupManagers groupManagers, String groupName) {
+    AllGroupManagers allGroupManagers = getAllGroupManagers();
     allGroupManagers.getGroupManagers().put(groupName, groupManagers);
     store.set(allGroupManagers);
   }
 
   public GroupManagers get(String groupName) {
-    return allGroupManagers.getGroupManagers().get(groupName);
+    return getAllGroupManagers().getGroupManagers().get(groupName);
   }
 
   public boolean isPermitted(String groupName) {
@@ -39,5 +48,21 @@ public class GroupManagerService {
 
   public void checkPermission(String groupName) {
     SecurityUtils.getSubject().checkPermission(PERMISSION + ":" + groupName);
+  }
+
+  /**
+   * Return the Groups managed from the given user
+   *
+   * @param username
+   * @return the Groups managed from the given user
+   */
+  public List<String> getManagedGroups(String username) {
+    List<String> result = new ArrayList<>();
+    getAllGroupManagers().getGroupManagers().forEach((groupName, groupManagers) -> {
+      if (groupManagers.getManagers().stream().anyMatch(s -> s.equals(username))) {
+        result.add(groupName);
+      }
+    });
+    return result;
   }
 }
---
+++ src/test/java/sonia/scm/groupmanager/GroupManagerRealmTest.java
@@ -0,0 +1,42 @@
+package sonia.scm.groupmanager;
+
+import com.google.common.collect.Lists;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sonia.scm.groupmanager.service.GroupManagerService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GroupManagerRealmTest {
+
+  @Mock
+  GroupManagerService service;
+
+  @InjectMocks
+  GroupManagerRealm groupManagerRealm;
+
+  @Test
+  void shouldAddPermissionToStoredGroupManager() {
+    PrincipalCollection principals = new SimplePrincipalCollection("user_1", "DefaultRealm");
+    when(service.getManagedGroups("user_1")).thenReturn(Lists.newArrayList("group_1"));
+    AuthorizationInfo info = groupManagerRealm.doGetAuthorizationInfo(principals);
+    assertThat(info.getStringPermissions()).containsExactlyInAnyOrder("group:read,modify,manage:group_1", "group:list");
+  }
+
+  @Test
+  void shouldNotAddPermissionToNotStoredGroupManager() {
+    PrincipalCollection principals = new SimplePrincipalCollection("user_1", "DefaultRealm");
+    when(service.getManagedGroups("user_1")).thenReturn(Lists.newArrayList());
+    AuthorizationInfo info = groupManagerRealm.doGetAuthorizationInfo(principals);
+    assertThat(info.getStringPermissions()).isNull();
+  }
+
+}
